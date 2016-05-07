ae9048f6511766da93933a885bff44435ab0087b
particlemen <jorgebart14@gmail.com>
2016-05-07 20:06:26 -0300

    Decoracion clase abstracta

---
+++ src/sansapizza/DecoracionRegular.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sansapizza;
+
+/**
+ *
+ * @author maligno
+ */
+public class DecoracionRegular extends Decoracion{
+    private int precio;
+    private double popularidad;
+    public DecoracionRegular() {
+        precio = 444;
+        popularidad = 0.25;
+        
+        void 
+    }
+}
